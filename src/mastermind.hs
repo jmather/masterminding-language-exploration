c0a49d045c851214d5ee51eb3b151249dfe6e080
Jacob Mather <jmather@mashery.com>
2014-04-11 12:56:41 -0700

    Working haskell example

--- src/mastermind.hs
+++ src/mastermind.hs
@@ -1,5 +1,9 @@
 import System.Random
+import Data.List
 import System.IO.Unsafe
+import System.IO
+import qualified Data.Text as T
+import Text.Regex.Posix
 
 -- These are actually functions
 solutionLength = 4
@@ -12,7 +16,6 @@ choices = "ABCDEF"
 -- Library code
 gameIntro :: IO ()
 gameIntro = do
-    putStrLn ("")
     putStrLn ("Let's play Mastermind! The rules are easy, I promise.")
     putStrLn ("I will pick " ++ (show solutionLength) ++ " letters out of a possible " ++ (show (length choices)) ++ ".")
     putStrLn ("You will then get " ++ (show maxGuesses) ++ " chances to guess which letters I picked.")
@@ -23,20 +26,85 @@ gameIntro = do
     putStrLn ""
 
 
-fixChar :: [Char] -> Char
-fixChar [val] = val
+randomStr :: (RandomGen g) => g -> [Char]
+randomStr g = do
+    take 1 $ randomRs ('A','F') $ unsafePerformIO newStdGen :: [Char]
 
-randomStr :: [Char]
-randomStr = take 10 $ randomRs ('A','F') (mkStdGen 3) :: [Char]
+generateSolutionWorker :: (Num a, Eq a) => a -> [Char] -> StdGen -> [Char]
+generateSolutionWorker 0 acc@(h:t) g = acc
+generateSolutionWorker n acc g
+    | letter `isInfixOf` acc = generateSolutionWorker n acc g
+    | otherwise = generateSolutionWorker (n - 1) (letter ++ acc) g
+    where letter = (randomStr g)
 
-generateSolution len acc
-    | (length acc) == len = acc
-    | len > 0 = do
-        strList <- randomStr
-        generateSolution len (randomStr ++ acc)
-    | otherwise = generateSolution solutionLength []
+generateSolution :: StdGen -> [Char]
+generateSolution g = generateSolutionWorker solutionLength "" g
 
-main :: IO StdGen -> IO ()
-main = do
+
+isValidGuess :: String -> Bool
+isValidGuess guess
+    | isTotalMatch = True
+    | otherwise = False
+    where pat = "[^" ++ (choices) ++ "]"
+          isTotalMatch = isMatch == False && rightLength
+          isMatch = (guess =~ pat) :: Bool
+          rightLength = (length guess) == (solutionLength)
+
+
+getGuess :: IO String
+getGuess = do
+    guess <- askGuess
+    if (isValidGuess guess)
+        then return guess
+        else do
+            putStrLn "We were unable to understand your input. Please enter only the letters of your guess and press enter."
+            getGuess
+
+askGuess :: IO String
+askGuess = do
+    let choiceList = [(head choices)] ++ (concat [", " ++ [x] | x <- (tail choices)])
+    putStrLn ("Available pegs: " ++ choiceList)
+    putStr ("Enter your guess, (pick " ++ (show solutionLength) ++ "): ")
+    hFlush stdout
+    line <- getLine
+    return $ T.unpack $ T.toUpper $ T.strip $ T.pack line
+
+
+analyzeGuessWorker :: String -> String -> String -> String -> String
+analyzeGuessWorker _ [] _ acc = reverse acc
+analyzeGuessWorker _ _ [] acc = reverse acc
+analyzeGuessWorker solution (solutionLetter : solutionRest) (guessLetter : guessRest) acc
+    | isSame = analyzeGuessWorker solution solutionRest guessRest (guessRight ++ acc)
+    | isInSolution = analyzeGuessWorker solution solutionRest guessRest (guessPresent ++ acc)
+    | otherwise = analyzeGuessWorker solution solutionRest guessRest (guessWrong ++ acc)
+    where isSame = guessLetter == solutionLetter
+          isInSolution = [guessLetter] `isInfixOf` solution
+
+
+analyzeGuess :: String -> String -> String
+analyzeGuess [] _ = []
+analyzeGuess _ [] = []
+analyzeGuess solution guess = analyzeGuessWorker solution solution guess []
+
+startGame :: StdGen -> IO ()
+startGame g = do
     gameIntro
-    putStrLn generateSolution newStdGen
+    let solution = (generateSolution g)
+    gameLoop solution maxGuesses
+
+
+gameLoop :: String -> Integer -> IO ()
+gameLoop solution 0 = putStrLn "You've ran out of guesses! Better luck next time!"
+gameLoop solution iteration = do
+    guess <- getGuess
+    if guess == solution
+        then putStrLn "You've guessed correctly! You win!"
+        else do
+            putStrLn ("Result: " ++ (analyzeGuess solution guess))
+            gameLoop solution (iteration - 1)
+
+
+main:: IO ()
+main = do
+    g <- newStdGen
+    startGame g
